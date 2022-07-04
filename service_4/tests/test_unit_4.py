6daa0d1805c312a22c9f306027fd43c1b94b8bf8
JackB <jackbarclay@live.com>
2022-07-04 16:38:02 +0000

    Solved testing issues

--- service_4/tests/test_unit_4.py
+++ service_4/tests/test_unit_4.py
@@ -9,13 +9,20 @@ class TestBase(TestCase):
         return app 
 
 class TestRank(TestBase):
-    def test_rank(self):
+    def test_rank1(self):
         response =self.client.post(url_for('get_rank'), json={'chapter':'Salamanders', 'service':'40'})
         self.assert200(response)
         self.assertIn(b'100', response.data)
 
-class TestRank(TestBase):
-    def test_rank(self):
+    def test_rank2(self):
         response =self.client.post(url_for('get_rank'), json={'chapter':'Dark Angels', 'service':'10'})
         self.assert200(response)
         self.assertIn(b'20', response.data)
+
+    def test_rank3(self):
+        response =self.client.post(url_for('get_rank'), json={'chapter':'Blood Angels', 'service':'80'})
+        self.assert200(response)
+        self.assertIn(b'130', response.data)
+
+
+    #The reason not 100% is ADD MORE HAHAHA YASSS
